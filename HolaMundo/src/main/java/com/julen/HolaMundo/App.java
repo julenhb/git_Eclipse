024d0194fb358f1f1bc4ba9b1c78f8f6dd80b1ca
julen <julen@DESKTOP-PC31G6F.mshome.net>
2022-11-16 09:52:42 +0100

    Commit inical del proyecto

---
+++ HolaMundo/src/main/java/com/julen/HolaMundo/App.java
@@ -0,0 +1,13 @@
+package com.julen.HolaMundo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
